d20f61bb8fa9355451fbb47638cf446e54f095e9
Navak94 <thenhindman@gmail.com>
2024-10-16 21:15:54 -0400

    made a bash file for running teleop

--- my_ros_env/bin/activate.fish
+++ my_ros_env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/navak/Desktop/ros2_ws/my_ros_env"
+set -gx VIRTUAL_ENV "/home/navak/Desktop/RBE-CS-526-master/my_ros_env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
