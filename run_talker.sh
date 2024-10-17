f8d9cda549dd1bf33de8110e6863a380c1952a7a
Navak94 <thenhindman@gmail.com>
2024-10-16 21:27:39 -0400

    made more bash for the other files

---
+++ run_talker.sh
@@ -0,0 +1,5 @@
+python3 -m venv ~/Desktop/RBE-CS-526-master/my_ros_env
+cd ~/Desktop/RBE-CS-526-master/src
+source ~/Desktop/RBE-CS-526-master/my_ros_env/bin/activate
+source /opt/ros/jazzy/setup.bash
+python3 talker.py
